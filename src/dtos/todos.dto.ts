4331bfa7a7502b3cb2c12f584ae53197844f1334
kwonsoonho02 <53741635+kwonsoonho02@users.noreply.github.com>
2023-07-27 16:38:01 +0900

    Feat:DTO addition

---
+++ src/dtos/todos.dto.ts
@@ -0,0 +1,29 @@
+import {IsNotEmpty, IsString, MinLength, MaxLength} from 'class-validator';
+
+export class CreateTodoDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(1)
+    @MaxLength(15)
+    public title : string
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(10)
+    @MaxLength(50)
+    public content : string
+}
+
+export class UpdateTodoDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(1)
+    @MaxLength(15)
+    public title : string
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(10)
+    @MaxLength(50)
+    public content : string
+}
