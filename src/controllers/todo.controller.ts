ad63bac14deac2bd5dfaf013a8863bd9bd197b1c
kwonsoonho02 <53741635+kwonsoonho02@users.noreply.github.com>
2023-07-27 16:39:47 +0900

    Feat: get, create, update, delete code modify and add

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,26 +1,30 @@
+import { CreateTodoDto, UpdateTodoDto } from '@/dtos/todos.dto';
+import { RequestWithUser } from '@/interfaces/auth.interface';
 import { TodoService } from '@/services/todo.service';
 import { NextFunction, Request, Response } from 'express';
 import Container from 'typedi';
+import { Todo } from '@/interfaces/todo.interface';
 
 export class TodoListController {
     public todo = Container.get(TodoService)
 
-    public getTodo = async (req : Request, res : Response, next : NextFunction) => {
+    public getTodo = async (req : RequestWithUser, res : Response, next : NextFunction) => {
         try{
-            const userId = Number(req.params.id);
-            console.log(userId)
-            const findAllTodoListData = await this.todo.findAllTodoList(userId);
+            const user = req.user.id
+            const findAllTodoListData : Todo[] = await this.todo.findAllTodoList(user);
+
             res.status(200).json({ data : findAllTodoListData, message : "findAll"})
         }catch (error) {
             next(error)
         }
     }
 
-    public createTodo = async (req : Request, res : Response, next : NextFunction) => {
+    public createTodo = async (req : RequestWithUser, res : Response, next : NextFunction) => {
         try{
-            const todoData = req.body;
-            const userId = Number(req.params.id);
-            const createTodoData = await this.todo.createTodo(todoData);
+            const todoData : CreateTodoDto = req.body;
+            const user = req.user;
+            console.log(user)
+            const createTodoData : Todo = await this.todo.createTodo(todoData, user.id);
 
             res.status(200).json({data : createTodoData, msg : "create"});
         }catch(error){
@@ -28,22 +32,24 @@ export class TodoListController {
         }
     }
 
-    public updateTodo = async (req: Request, res: Response, next: NextFunction) => {
+    public updateTodo = async (req: RequestWithUser, res: Response, next: NextFunction) => {
         try {
-          const todoId = Number(req.params.id);
-          const todoData = req.body;
-          const updateTodoData = await this.todo.updateTodo(todoId, todoData);
-      
+            const user = req.user;
+            const todoId = Number(req.params.id);
+            const todoData : UpdateTodoDto = req.body;
+
+            const updateTodoData : number[] = await this.todo.updateTodo(user.id, todoId,todoData);
           res.status(200).json({ data: updateTodoData, message: 'updated' });
         } catch (error) {
           next(error);
         }
     }
 
-    public deleteTodo = async (req : Request, res : Response, next : NextFunction) => {
+    public deleteTodo = async (req : RequestWithUser, res : Response, next : NextFunction) => {
         try {
+            const user = req.user;
             const todoId = Number(req.params.id);
-            const deleteTodoData = await this.todo.deleteTodo(todoId);
+            const deleteTodoData : number = await this.todo.deleteTodo(user.id, todoId);
 
             res.status(200).json({ data: deleteTodoData, message: 'delete' });
         }catch (error){
