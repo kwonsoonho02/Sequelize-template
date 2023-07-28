862d61572c61bf31758bd981909273b84c200a39
kwonsoonho02 <53741635+kwonsoonho02@users.noreply.github.com>
2023-07-28 13:09:37 +0900

    Refactor:database env add

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,18 +1,24 @@
-import Sequelize from 'sequelize';
-import { NODE_ENV, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } from '@config';
-import UserModel from '@models/users.model';
-import TodoModel from '@models/todos.model'
-import { logger } from '@utils/logger';
+import Sequelize from "sequelize";
+import {
+  NODE_ENV,
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_DATABASE,
+} from "@config";
+import UserModel from "@models/users.model";
+import TodoModel from "@models/todos.model";
+import { logger } from "@utils/logger";
 
-
-const sequelize = new Sequelize.Sequelize('todolist', 'root', '0414', {
-  dialect: 'mariadb',
+const sequelize = new Sequelize.Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
+  dialect: "mariadb",
   host: DB_HOST,
   port: DB_PORT,
-  timezone: '+09:00',
+  timezone: "+09:00",
   define: {
-    charset: 'utf8mb4',
-    collate: 'utf8mb4_general_ci',
+    charset: "utf8mb4",
+    collate: "utf8mb4_general_ci",
     underscored: true,
     freezeTableName: true,
   },
@@ -20,9 +26,9 @@ const sequelize = new Sequelize.Sequelize('todolist', 'root', '0414', {
     min: 0,
     max: 5,
   },
-  logQueryParameters: NODE_ENV === 'development',
+  logQueryParameters: NODE_ENV === "development",
   logging: (query, time) => {
-    logger.info(time + 'ms' + ' ' + query);
+    logger.info(time + "ms" + " " + query);
   },
   benchmark: true,
 });
@@ -33,9 +39,12 @@ const TodoLists = TodoModel(sequelize);
 
 // 관계 설정
 Users.hasMany(TodoLists, {
-  foreignKey: 'userId',
-  sourceKey: 'id',
-  onDelete: 'CASCADE',
+  foreignKey: "userId",
+  sourceKey: "id",
+  onDelete: "CASCADE",
+});
+TodoLists.belongsTo(Users, {
+  foreignKey: "userId",
 });
 
 // 이후 DB 객체에 등록하여 내보냅니다.
