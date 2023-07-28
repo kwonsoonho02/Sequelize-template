e06f00513140ae7f686cfc3a43fe125c5cfd72a6
kwonsoonho02 <53741635+kwonsoonho02@users.noreply.github.com>
2023-07-28 13:13:59 +0900

    Refactor: update todo affectCount variable

--- src/services/todo.service.ts
+++ src/services/todo.service.ts
@@ -1,40 +1,56 @@
-import { hash } from 'bcrypt';
-import { Service } from 'typedi';
-import { DB } from '@database';
-import { HttpException } from '@/exceptions/httpException';
-import { Todo } from '@/interfaces/todo.interface';
-import { User } from '@interfaces/users.interface';
-import { promises } from 'dns';
-import { signedCookies } from 'cookie-parser';
-import { where } from 'sequelize';
-import { CreateTodoDto } from '@/dtos/todos.dto';
-import { UpdateTodoDto } from '@/dtos/todos.dto';
-
+import { hash } from "bcrypt";
+import { Service } from "typedi";
+import { DB } from "@database";
+import { HttpException } from "@/exceptions/httpException";
+import { Todo } from "@/interfaces/todo.interface";
+import { User } from "@interfaces/users.interface";
+import { promises } from "dns";
+import { signedCookies } from "cookie-parser";
+import { where } from "sequelize";
+import { CreateTodoDto } from "@/dtos/todos.dto";
+import { UpdateTodoDto } from "@/dtos/todos.dto";
 
 @Service()
 export class TodoService {
-    public async findAllTodoList(userId : number): Promise<Todo[]> {
-        const allTodoList: Todo[] = await DB.TodoLists.findAll({where: {userId}});
+  public async findAllTodoList(userId: number): Promise<Todo[]> {
+    const allTodoList: Todo[] = await DB.TodoLists.findAll({
+      where: { userId },
+    });
 
-        return allTodoList;
-    }
+    return allTodoList;
+  }
 
-    public async createTodo(todoData : CreateTodoDto, userId : number) : Promise<Todo> {
-        console.log(todoData)
-        const createTodoData : Todo = await DB.TodoLists.create({...todoData, userId});
+  public async createTodo(
+    todoData: CreateTodoDto,
+    userId: number
+  ): Promise<Todo> {
+    console.log(todoData);
+    const createTodoData: Todo = await DB.TodoLists.create({
+      ...todoData,
+      userId,
+    });
 
-        return createTodoData
-    }
+    return createTodoData;
+  }
 
-    public async updateTodo (userId : number, todoId : number, todoData : UpdateTodoDto) : Promise<number[]>{
-        const updateTodoData : number[] = await DB.TodoLists.update(todoData, { where : {userId, id : todoId}})
-    
-        return updateTodoData;
-    }
+  public async updateTodo(
+    userId: number,
+    todoId: number,
+    todoData: UpdateTodoDto
+  ): Promise<boolean> {
+    const [affectCount] = await DB.TodoLists.update(todoData, {
+      where: { userId, id: todoId },
+    });
 
-    public async deleteTodo (userId : number, todoId : number) : Promise<number> {
-        const deleteTodoData : number = await DB.TodoLists.destroy({where : {userId, id : todoId}})
-    
-        return deleteTodoData;
-    }
+    if (affectCount === 0) return false;
+    return true;
+  }
+
+  public async deleteTodo(userId: number, todoId: number): Promise<number> {
+    const deleteTodoData: number = await DB.TodoLists.destroy({
+      where: { userId, id: todoId },
+    });
+
+    return deleteTodoData;
+  }
 }
