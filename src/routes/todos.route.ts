f8ed048f192c55d076988172b8f0d6865b758902
kwonsoonho02 <53741635+kwonsoonho02@users.noreply.github.com>
2023-07-28 13:12:06 +0900

    Refactor: todo router middleware add

--- src/routes/todos.route.ts
+++ src/routes/todos.route.ts
@@ -1,13 +1,13 @@
-import { Router } from 'express';
-import { TodoListController } from '@controllers/todo.controller';
-import { CreateUserDto } from '@dtos/users.dto';
-import { Routes } from '@interfaces/routes.interface';
-import { ValidationMiddleware } from '@middlewares/validation.middleware';
-import { AuthMiddleware } from '@/middlewares/auth.middleware';
-import { CreateTodoDto } from '@/dtos/todos.dto';
+import { Router } from "express";
+import { TodoListController } from "@controllers/todo.controller";
+import { CreateUserDto } from "@dtos/users.dto";
+import { Routes } from "@interfaces/routes.interface";
+import { ValidationMiddleware } from "@middlewares/validation.middleware";
+import { AuthMiddleware } from "@/middlewares/auth.middleware";
+import { CreateTodoDto, UpdateTodoDto } from "@/dtos/todos.dto";
 
 export class TodoRoute implements Routes {
-  public path = '/todos';
+  public path = "/todos";
   public router = Router();
   public todo = new TodoListController();
 
@@ -17,8 +17,22 @@ export class TodoRoute implements Routes {
 
   private initializeRoutes() {
     this.router.get(`${this.path}`, AuthMiddleware, this.todo.getTodo);
-    this.router.post(`${this.path}`, AuthMiddleware, this.todo.createTodo);
-    this.router.put(`${this.path}/:id`, AuthMiddleware, this.todo.updateTodo);
-    this.router.delete(`${this.path}/:id`, AuthMiddleware, this.todo.deleteTodo);
+    this.router.post(
+      `${this.path}`,
+      ValidationMiddleware(CreateTodoDto, "body"),
+      AuthMiddleware,
+      this.todo.createTodo
+    );
+    this.router.put(
+      `${this.path}/:id`,
+      ValidationMiddleware(UpdateTodoDto, "body"),
+      AuthMiddleware,
+      this.todo.updateTodo
+    );
+    this.router.delete(
+      `${this.path}/:id`,
+      AuthMiddleware,
+      this.todo.deleteTodo
+    );
   }
 }
